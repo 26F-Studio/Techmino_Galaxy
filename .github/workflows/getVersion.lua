a981fa638af96a1b52d5ff7315f778df167ab1ef
ParticleG <particle_g@outlook.com>
2022-06-10 00:51:26 +0800

    - Fix auto test - Pack right files - Update getVersion.lua - Update filename

--- .github/workflows/getVersion.lua
+++ .github/workflows/getVersion.lua
@@ -3,26 +3,12 @@ if arg=="-apkCode"then
     local code=require"version".apkCode
     print(code)
 elseif arg=="-code"then
-    local str=require"version".code
+    local str=require"version".verCode
     print(str)
 elseif arg=="-name"then
-    local str=require"version".string
+    local str=require"version".appVer
     print(str)
 elseif arg=="-release"then
-    local str=require"version".string:gsub("V","",1)
+    local str=require"version".verStr
     print(str)
-elseif arg=="-updateTitle"then
-    local note=require"parts.updateLog"
-    local p1=note:find("\n%d")+1
-    local p2=note:find("\n",p1)-1
-    note=note:sub(p1,p2)
-    print(note)
-elseif arg=="-updateNote"then
-    local note=require"parts.updateLog"
-    local p1=note:find("\n",note:find("\n%d")+1)+1
-    local p2=note:find("\n%d",p1+1)
-    note=note:sub(p1,p2-2)
-        :gsub("                ","- ")
-        :gsub("        ","# ")
-    print(note)
 end
