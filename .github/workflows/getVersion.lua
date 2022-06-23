4aeb402531dc48254549b693e492b950a9dff8be
ParticleG <particle_g@outlook.com>
2022-06-24 00:07:37 +0800

    - Fix typo

--- .github/workflows/getVersion.lua
+++ .github/workflows/getVersion.lua
@@ -5,8 +5,8 @@ if arg[1] == "-appName" then
 elseif arg[1] == "-versionName" then
     res = version.appVer
 elseif arg[1] == "-versionString" then
-    res = require "version".verStr
+    res = version.verStr
 elseif arg[1] == "-versionCode" then
-    res = require "version".apkCode
+    res = version.apkCode
 end
 print(res)
