7703131faa9cc61e6f26de15f61802e66b0ca79b
MrZ_26 <1046101471@qq.com>
2022-08-08 02:36:23 +0800

    修正音乐室ui在长屏幕很奇怪

--- assets/scene/musicroom.lua
+++ assets/scene/musicroom.lua
@@ -1,5 +1,3 @@
-local gc=love.graphics
-
 local bigTitle=setmetatable({},{
     __index=function(self,name)
         local up=true
@@ -23,15 +21,15 @@ local musicListBox do
     musicListBox={type='listBox',pos={0,.5},x=100,y=-400,w=500,h=800,lineHeight=50}
     function musicListBox.drawFunc(name,n,sel)
         if sel then
-            gc.setColor(1,1,1,.26)
-            gc.rectangle('fill',0,0,500,50)
+            GC.setColor(1,1,1,.26)
+            GC.rectangle('fill',0,0,500,50)
         end
         FONT.set(30)
-        gc.setColor(COLOR.lS)
-        gc.printf(n,0,7,80,'center')
+        GC.setColor(COLOR.lS)
+        GC.printf(n,0,7,80,'center')
         FONT.set(40)
-        gc.setColor(COLOR.L)
-        gc.print(bigTitle[name],75,0)
+        GC.setColor(COLOR.L)
+        GC.print(bigTitle[name],75,0)
     end
     musicListBox=WIDGET.new(musicListBox)
 
@@ -89,25 +87,26 @@ function scene.keyDown(key,isRep)
 end
 
 function scene.draw()
+    GC.replaceTransform(SCR.xOy_l)
     local t=love.timer.getTime()
     FONT.set(90)
-    gc.setColor(math.sin(t*.5)*.2+.8,math.sin(t*.7)*.2+.8,math.sin(t)*.2+.8)
-    gc.print(bigTitle[selected],630,120)
+    GC.setColor(math.sin(t*.5)*.2+.8,math.sin(t*.7)*.2+.8,math.sin(t)*.2+.8)
+    GC.print(bigTitle[selected],630,-380)
 
     FONT.set(50)
-    gc.setColor(1,math.sin(t*2.6)*.5+.5,math.sin(t*2.6)*.5+.5)
-    gc.print(bgmList[selected].author,700,230)
+    GC.setColor(1,math.sin(t*2.6)*.5+.5,math.sin(t*2.6)*.5+.5)
+    GC.print(bgmList[selected].author,700,-270)
     if bgmList[selected].message then
         FONT.set(25)
-        gc.setColor(COLOR.LD)
-        gc.printf(bgmList[selected].message,650,320,800,'left')
+        GC.setColor(COLOR.LD)
+        GC.printf(bgmList[selected].message,650,-180,800,'left')
     end
 
     if BGM.tell() then
-        gc.replaceTransform(SCR.xOy_l)
+        GC.replaceTransform(SCR.xOy_l)
         FONT.set(20)
-        gc.setColor(COLOR.L)
-        gc.print(STRING.time_simp(BGM.tell()%BGM.getDuration()).." / "..STRING.time_simp(BGM.getDuration()),630,230)
+        GC.setColor(COLOR.L)
+        GC.print(STRING.time_simp(BGM.tell()%BGM.getDuration()).." / "..STRING.time_simp(BGM.getDuration()),630,230)
     end
 end
 
