4d6cd14fd6dcaaf767e7557b3c96e197c22fc7f7
MrZ_26 <1046101471@qq.com>
2022-09-11 07:55:09 +0800

    两个shader使用highp类型

--- assets/shader/rgb.glsl
+++ assets/shader/rgb.glsl
@@ -1,4 +1,4 @@
-uniform float phase;
+uniform highp float phase;
 uniform float rkx,rky,rb,ra,rot,rox,roy;
 uniform float gkx,gky,gb,ga,got,gox,goy;
 uniform float bkx,bky,bb,ba,bot,box,boy;
@@ -7,9 +7,9 @@ vec4 effect(vec4 color,sampler2D tex,vec2 texCoord,vec2 scrCoord){
     float x=scrCoord.x/love_ScreenSize.x;
     float y=scrCoord.y/love_ScreenSize.y;
     return vec4(
-        rkx*x+rky*y+rb+ra*sin(rot*phase+rox*x+roy*y),
-        gkx*x+gky*y+gb+ga*sin(got*phase+gox*x+goy*y),
-        bkx*x+bky*y+bb+ba*sin(bot*phase+box*x+boy*y),
+        rkx*x+rky*y+rb+ra*sin(phase*rot+rox*x+roy*y),
+        gkx*x+gky*y+gb+ga*sin(phase*got+gox*x+goy*y),
+        bkx*x+bky*y+bb+ba*sin(phase*bot+box*x+boy*y),
         a
     );
     // .8-y*.7+.2*sin(t/6.26),
