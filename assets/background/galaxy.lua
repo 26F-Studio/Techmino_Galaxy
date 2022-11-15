b1e1ae6bf961ce2ececb1366b8281ceac68d01e8
MrZ_26 <1046101471@qq.com>
2022-11-15 14:43:05 +0800

    删除旧的用shader的galaxy背景 添加新的简易手算3d的galaxy背景（space升级版）

--- assets/background/galaxy.lua
+++ assets/background/galaxy.lua
@@ -1,21 +1,38 @@
--- Super cool galaxy background
-local gc=love.graphics
+-- More realistic space background
+local ins,rem=table.insert,table.remove
+local rnd=math.random
+local circle=GC.circle
+local stars={}
+local camX,camY=0,0
+
 local back={}
-local shader=SHADER.galaxy
-shader:send('alpha',.626)
-local t
 
 function back.init()
-    t=math.random()*2600
+    for _=1,620 do
+        ins(stars,{
+            x=rnd()*2-1,y=rnd()*2-1,z=.26+rnd(),
+            r=1+1.5*rnd(),
+        })
+    end
 end
-function back.update(dt)
-    t=t+dt
+function back.update()
+    camX=.026*math.sin(love.timer.getTime()*.26)
+    camY=.012*math.sin(love.timer.getTime()*.62)
 end
 function back.draw()
-    gc.clear(.08,.08,.084)
-    shader:send('time',t)
-    gc.setShader(shader)
-    gc.rectangle('fill',0,0,SCR.w,SCR.h)
-    gc.setShader()
+    GC.clear(.06,.06,.06)
+    local w,h=SCR.w,SCR.h
+    GC.translate(w/2,h/2)
+    GC.setColor(1,1,1)
+    for i=1,620 do
+        local s=stars[i]
+        local x=(s.x-camX)*1/(1+s.z)
+        local y=(s.y-camY)*1/(1+s.z)
+        local r=s.r*1/(1+s.z)
+        circle('fill',x*w,y*h,r)
+    end
+end
+function back.discard()
+    stars={}
 end
 return back
