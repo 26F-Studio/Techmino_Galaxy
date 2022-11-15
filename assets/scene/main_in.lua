e80280dd14170fac35c7eccc83aaac4ea72d4985
MrZ_26 <1046101471@qq.com>
2022-11-15 18:34:43 +0800

    移除12阶段主菜单音乐室入口

--- assets/scene/main_in.lua
+++ assets/scene/main_in.lua
@@ -39,13 +39,13 @@ scene.widgetList={
     WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=-270,y=140,w=500,h=140, color='B',text=LANG'main_in_tutorial',fontSize=40,cornerR=0,code=WIDGET.c_goScn'tutorial'},
     WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=270, y=140,w=500,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=WIDGET.c_goScn'sandbox_1'},
 
+
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=-200,y=-40,w=360,h=140, color='R',text=LANG'main_in_sprint',  fontSize=40,cornerR=0,code=playMode'mino/sprint'},
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=200, y=-40,w=360,h=140, color='R',text=LANG'main_in_marathon',fontSize=40,cornerR=0,code=playMode'mino/marathon'},
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=-200,y=140,w=360,h=140, color='B',text=LANG'main_in_tutorial',fontSize=40,cornerR=0,code=WIDGET.c_goScn'tutorial'},
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=200, y=140,w=360,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=WIDGET.c_goScn'sandbox_1'},
 
-    WIDGET.new{type='button',     pos={.5,.5},x=-400,y=320,w=360,h=100,text=CHAR.icon.language,               fontSize=70,cornerR=0,code=WIDGET.c_goScn'setting_lang'},
-    WIDGET.new{type='button',     pos={.5,.5},x=0,   y=320,w=360,h=100,text=LANG'main_in_settings',           fontSize=40,cornerR=0,code=WIDGET.c_goScn'setting_in'},
-    WIDGET.new{type='button',     pos={.5,.5},x=400, y=320,w=360,h=100,text=LANG'main_in_musicroom',          fontSize=40,cornerR=0,code=WIDGET.c_goScn'musicroom'},
+    WIDGET.new{type='button',     pos={.5,.5},x=-270,y=320,w=400,h=100,text=CHAR.icon.language,               fontSize=70,cornerR=0,code=WIDGET.c_goScn'setting_lang'},
+    WIDGET.new{type='button',     pos={.5,.5},x=270, y=320,w=400,h=100,text=LANG'main_in_settings',           fontSize=40,cornerR=0,code=WIDGET.c_goScn'setting_in'},
 }
 return scene
