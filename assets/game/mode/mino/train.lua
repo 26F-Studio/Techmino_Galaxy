98bb463a631f52477730f4bd555d56e332e43bc1
MrZ_26 <1046101471@qq.com>
2022-11-16 01:59:21 +0800

    一二阶段主菜单的沙盒模式按钮导向train模式，具体玩法待实现

---
+++ assets/game/mode/mino/train.lua
@@ -0,0 +1,19 @@
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+    end,
+    settings={mino={
+        dropDelay=1e99,
+        lockDelay=1e99,
+        infHold=true,
+        soundEvent={
+            countDown=function(num)
+                SFX.playSample('lead',num>0 and 'A3' or 'A4')
+            end,
+        },
+        event={
+            -- TODO: display ghost at not-bad places to help new players learn stacking
+        },
+    }},
+}
--- assets/scene/main_in.lua
+++ assets/scene/main_in.lua
@@ -37,13 +37,13 @@ scene.widgetList={
     WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=-270,y=-40,w=500,h=140, color='R',text=LANG'main_in_sprint',  fontSize=40,cornerR=0,code=playMode'mino/sprint'},
     WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=270, y=-40,w=500,h=140, color='R',text=LANG'main_in_marathon',fontSize=40,cornerR=0,code=playMode'mino/marathon'},
     WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=-270,y=140,w=500,h=140, color='B',text=LANG'main_in_tutorial',fontSize=40,cornerR=0,code=WIDGET.c_goScn'tutorial'},
-    WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=270, y=140,w=500,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=WIDGET.c_goScn'sandbox_1'},
+    WIDGET.new{name='1',type='button_fill',pos={.5,.5},x=270, y=140,w=500,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=playMode'mino/train'},
 
 
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=-200,y=-40,w=360,h=140, color='R',text=LANG'main_in_sprint',  fontSize=40,cornerR=0,code=playMode'mino/sprint'},
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=200, y=-40,w=360,h=140, color='R',text=LANG'main_in_marathon',fontSize=40,cornerR=0,code=playMode'mino/marathon'},
     WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=-200,y=140,w=360,h=140, color='B',text=LANG'main_in_tutorial',fontSize=40,cornerR=0,code=WIDGET.c_goScn'tutorial'},
-    WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=200, y=140,w=360,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=WIDGET.c_goScn'sandbox_1'},
+    WIDGET.new{name='2',type='button_fill',pos={.5,.5},x=200, y=140,w=360,h=140, color='Y',text=LANG'main_in_sandbox', fontSize=40,cornerR=0,code=playMode'mino/train'},
 
     WIDGET.new{type='button',     pos={.5,.5},x=-270,y=320,w=400,h=100,text=CHAR.icon.language,               fontSize=70,lineWidth=4,cornerR=0,code=WIDGET.c_goScn'lang_in'},
     WIDGET.new{type='button',     pos={.5,.5},x=270, y=320,w=400,h=100,text=LANG'main_in_settings',           fontSize=40,lineWidth=4,cornerR=0,code=WIDGET.c_goScn'setting_in'},
