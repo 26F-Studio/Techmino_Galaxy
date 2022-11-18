2dbae33355dd72e496ec6df2d26098759f56712b
MrZ_26 <1046101471@qq.com>
2022-11-19 02:05:22 +0800

    修改1/2阶段练习模式的提示落块位置样式，关闭场地晃动 框架跟进

--- assets/game/mode/mino/interior/train.lua
+++ assets/game/mode/mino/interior/train.lua
@@ -5,10 +5,11 @@ return {
     end,
     settings={mino={
         skin='mino_interior',
+        shakeness=0,
         dropDelay=1e99,
         lockDelay=1e99,
         infHold=true,
-        spawnDelay=100,
+        -- spawnDelay=26,
         soundEvent={
             countDown=function(num)
                 SFX.playSample('lead',num>0 and 'A3' or 'A4')
@@ -33,17 +34,18 @@ return {
                 P.modeData.x,P.modeData.y,P.modeData.shape=x,y,TABLE.rotate(shape,dir)
                 P.modeData.waitTime=0
                 -- if dir~='0' then P:rotate(dir) end
-                -- P.handX,P.handY=P.modeData.x,P.modeData.y
+                -- P.handX,P.handY,P.ghostY=P.modeData.x,P.modeData.y,P.modeData.y
                 -- P:minoDropped()
             end,
             drawBelowMarks=function(P)
                 local m=P.modeData.shape
                 if type(m)=='table' then
-                    GC.setColor(1,1,1,math.min(P.modeData.waitTime/260,1)*.26-.1*math.cos(P.modeData.waitTime*.006))
+                    GC.setColor(1,1,1,.26*(math.min(P.modeData.waitTime/62,1)-math.cos(P.modeData.waitTime*.012)))
+                    GC.setLineWidth(6)
                     for y=1,#m do for x=1,#m[1] do
                         local C=m[y][x]
                         if C then
-                            GC.rectangle('fill',(P.modeData.x+x-2)*40,-(P.modeData.y+y-1)*40,40,40)
+                            GC.rectangle('line',(P.modeData.x+x-2)*40+5,-(P.modeData.y+y-1)*40+5,30,30)
                         end
                     end end
                 end
