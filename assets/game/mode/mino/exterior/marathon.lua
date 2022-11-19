4487d0f4ee92811a162562c55f8cf087a49167dc
MrZ_26 <1046101471@qq.com>
2022-11-20 02:13:10 +0800

    3/4阶段主菜单俩按钮先暂时作为exterior的40行和马拉松入口方便测试

---
+++ assets/game/mode/mino/exterior/marathon.lua
@@ -0,0 +1,45 @@
+local dropSpeed={1000,833,666,500,400,300,200,150,100,70,50,42,30,16,12,8,7,6,5,4}
+local gc=love.graphics
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('propel','base')
+    end,
+    settings={mino={
+        das=120,
+        arr=16,
+        sdarr=16,
+        spawnDelay=130,
+        clearDelay=300,
+        event={
+            playerInit=function(P)
+                P.settings.dropDelay=dropSpeed[1]
+                P.modeData.target=10
+            end,
+            afterClear=function(P)
+                P.modeData.line=math.min(P.modeData.line+P.clearHistory[#P.clearHistory].line,200)
+                if P.modeData.line>=P.modeData.target then
+                    if P.modeData.target<200 then
+                        if PROGRESS.getMain()>=2 and P.modeData.target<=150 and P.isMain then
+                            BGM.set(bgmList['propel'].add,'volume',(P.modeData.target/150)^2,2.6)
+                        end
+                        P.settings.dropDelay=dropSpeed[P.modeData.target/10+1]
+                        P.modeData.target=P.modeData.target+10
+                        P:playSound('reach')
+                    else
+                        P:finish('AC')
+                    end
+                end
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(P.modeData.line,-300,-110)
+                gc.rectangle('fill',-380,-2,160,4)
+                GC.mStr(P.modeData.target,-300,0)
+            end,
+        },
+    }},
+}
---
+++ assets/game/mode/mino/exterior/sprint.lua
@@ -0,0 +1,34 @@
+local gc=love.graphics
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('race','base')
+        BG.set('none')
+    end,
+    settings={mino={
+        dropDelay=1000,
+        lockDelay=1000,
+        event={
+            afterClear=function(P)
+                P.modeData.line=math.min(P.modeData.line+P.clearHistory[#P.clearHistory].line,40)
+                if P.modeData.line>=40 then
+                    P:finish('AC')
+                end
+                if PROGRESS.getMain()>=2 and P.modeData.line>10 and P.isMain then
+                    BGM.set(bgmList['race'].add,'volume',math.min((P.modeData.line-10)/20,1),2.6)
+                end
+            end,
+            drawInField=function(P)
+                gc.setColor(1,1,1,.26)
+                gc.rectangle('fill',0,(P.modeData.line-40)*40-2,P.settings.fieldW*40,4)
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(40-P.modeData.line,-300,-55)
+            end,
+        },
+    }},
+}
