3eb21927451cde054ff073baf82fef6cd2af69cb
MrZ_26 <1046101471@qq.com>
2022-11-20 02:02:26 +0800

    删除1/2阶段的40行的剩余行数指示线

--- assets/game/mode/mino/interior/sprint.lua
+++ assets/game/mode/mino/interior/sprint.lua
@@ -34,10 +34,6 @@ return {
                     BGM.set(bgmList['race'].add,'volume',math.min((P.modeData.line-10)/20,1),2.6)
                 end
             end,
-            drawInField=function(P)
-                gc.setColor(1,1,1,.26)
-                gc.rectangle('fill',0,(P.modeData.line-40)*40-2,P.settings.fieldW*40,4)
-            end,
             drawOnPlayer=function(P)
                 gc.setColor(COLOR.L)
                 FONT.set(80)
