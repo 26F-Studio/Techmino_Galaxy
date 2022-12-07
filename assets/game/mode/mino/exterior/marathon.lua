c771a55269b89d2279934c1809456741ab1579e3
MrZ_26 <1046101471@qq.com>
2022-12-08 02:51:16 +0800

    大致实现exterior马拉松的数据分析图表

--- assets/game/mode/mino/exterior/marathon.lua
+++ assets/game/mode/mino/exterior/marathon.lua
@@ -49,8 +49,12 @@ return {
                 P.settings.dropDelay=levels[1].drop
                 P.settings.lockDelay=levels[1].lock
                 P.settings.spawnDelay=levels[1].spawn
+
+                P.modeData.transition1=1e99
+                P.modeData.transition2=1e99
                 P.modeData.line=0
                 P.modeData.target=10
+
                 P.modeData.level=1
                 P.modeData.levelStartTime=0
                 P.modeData.levelPieces=0
@@ -67,7 +71,13 @@ return {
                         while 1/(levels[autoLevel].drop*15)<averagePieceTime do
                             autoLevel=autoLevel+1
                         end
-                        P.modeData.level=math.min(math.max(P.modeData.level+1,math.min(P.modeData.level+5,autoLevel-5)),30)
+                        local _level=P.modeData.level
+                        P.modeData.level=math.min(math.max(P.modeData.level+1,math.min(P.modeData.level+4,autoLevel-5)),30)
+                        if _level<=10 and P.modeData.level>10 then
+                            P.modeData.transition1=#P.dropHistory
+                        elseif _level<=20 and P.modeData.level>20 then
+                            P.modeData.transition2=#P.dropHistory
+                        end
                         P.settings.dropDelay=levels[P.modeData.level].drop
                         P.settings.lockDelay=levels[P.modeData.level].lock
                         P.settings.spawnDelay=levels[P.modeData.level].spawn
@@ -97,4 +107,72 @@ return {
             end,
         },
     }},
+    result=function()
+        local P=GAME.mainPlayer
+        if not P then return end
+        if P.modeData.line<40 then return end
+
+        local dropInfo={}
+
+        local finalTime=P.time-3000
+        local finRate=P.modeData.line/200
+
+        for i,d in next,P.dropHistory do
+            table.insert(dropInfo,{
+                x=(d.time-3000)/finalTime*finRate,
+                y=i/#P.dropHistory*finRate,
+            })
+        end
+
+        P.modeData.finalTime=finalTime
+        P.modeData.dropInfo=dropInfo
+    end,
+    resultPage=function(time)
+        local P=GAME.mainPlayer
+        if not P then return end
+        if not P.modeData.finalTime then
+            FONT.set(100)
+            GC.setColor(1,1,1,math.min(time*2.6,1))
+            GC.mStr(P.modeData.line.." / 200",800,400)
+            return
+        end
+
+        local t=MATH.expApproach(0,1,time^2*26)
+        local maxH=600*MATH.expApproach(0,1,math.max(time-.26,0)^2*12.6)
+
+        -- Set axis' trasformation
+        GC.translate(400,800)
+        GC.scale(1,-1)
+
+        -- Piece-time
+        local dropData=P.modeData.dropInfo
+        lastX,lastY=0,0
+        for i=1,#dropData do
+            local clr=i>P.modeData.transition2 and COLOR.R or i>P.modeData.transition1 and COLOR.Y or COLOR.G
+            -- Fill
+            GC.setColor(clr[1],clr[2],clr[3],.6)
+            GC.polygon('fill',
+                800*t*lastX,0,
+                800*t*lastX,lastY*maxH,
+                800*t*dropData[i].x,dropData[i].y*maxH,
+                800*t*dropData[i].x,0
+            )
+            -- Line
+            GC.setLineWidth(2)
+            GC.setColor(clr)
+            GC.line(
+                800*t*lastX,lastY*maxH,
+                800*t*dropData[i].x,dropData[i].y*maxH
+            )
+            lastX,lastY=dropData[i].x,dropData[i].y
+        end
+
+        -- Axis
+        GC.setLineWidth(2)
+        GC.setColor(COLOR.dL)
+        GC.line(0,600*t,0,0,800*t,0)
+        FONT.set(30)
+        GC.setColor(1,1,1,t)
+        GC.printf(STRING.time(P.modeData.finalTime/1000),800*t-260,-10,260,'right',nil,1,-1)
+    end,
 }
