352b822f9c15a64e230836c9bbd8acfc0d081e60
MrZ_26 <1046101471@qq.com>
2022-12-07 11:51:59 +0800

    修改interior挖掘模式垃圾行生成规则，开始必定在中间四列，略微减少偶然性

--- assets/game/mode/mino/interior/dig.lua
+++ assets/game/mode/mino/interior/dig.lua
@@ -17,8 +17,8 @@ return {
         },
         event={
             playerInit=function(P)
-                local phase=P.seqRND:random(0,9)
                 local dir=P.seqRND:random(0,1)*2-1
+                local phase=P.seqRND:random(3,6)
                 for i=1,12 do P:riseGarbage((phase+i)*dir%10+1) end
                 P.fieldDived=0
                 P.modeData.garbageRemain=12
