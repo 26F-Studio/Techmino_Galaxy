44fc82b2efaa16d7f5c6c61a4c548942330554e0
MrZ_26 <1046101471@qq.com>
2022-12-31 02:21:15 +0800

    添加翻转40行模式

---
+++ assets/game/mode/mino/exterior/sprint_flip_40.lua
@@ -0,0 +1,40 @@
+local gc=love.graphics
+local lineTarget=40
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('race','base')
+    end,
+    settings={mino={
+        event={
+            playerInit=function(P)
+                P.modeData.line=0
+            end,
+            afterLock=function(P)
+                for y=1,P.field:getHeight() do
+                    TABLE.reverse(P.field._matrix[y])
+                end
+            end,
+            afterClear=function(P,movement)
+                P.modeData.line=math.min(P.modeData.line+#movement.clear,lineTarget)
+                if P.modeData.line>=lineTarget then
+                    P:finish('AC')
+                end
+                if P.modeData.line>10 and P.modeData.line<34 and P.isMain then
+                    BGM.set(bgmList['race'].add,'volume',math.min((P.modeData.line-10)/20,1),2.6)
+                end
+            end,
+            drawInField=function(P)
+                gc.setColor(1,1,1,.26)
+                gc.rectangle('fill',0,(P.modeData.line-lineTarget)*40-2,P.settings.fieldW*40,4)
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(lineTarget-P.modeData.line,-300,-55)
+            end,
+        },
+    }},
+}
