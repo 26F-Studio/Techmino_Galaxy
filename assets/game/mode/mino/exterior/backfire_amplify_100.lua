2241b5eafbd34192d0cdb2618bcc9fdca984d9bf
MrZ_26 <1046101471@qq.com>
2023-01-03 19:03:04 +0800

    添加回火和放大回火模式

---
+++ assets/game/mode/mino/exterior/backfire_amplify_100.lua
@@ -0,0 +1,44 @@
+local gc=love.graphics
+local lineTarget=100
+local bgmTransBegin,bgmTransFinish=50,75
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('echo','base')
+    end,
+    settings={mino={
+        atkSys='nextgen',
+        allowCancel=true,
+        clearStuck=true,
+        event={
+            playerInit=function(P)
+                P.modeData.line=0
+            end,
+            beforeSend=function(P,atk)
+                atk.power=atk.power*3
+                atk.defendRate=2
+                P:receive(atk)
+            end,
+            afterClear=function(P,movement)
+                P.modeData.line=math.min(P.modeData.line+#movement.clear,lineTarget)
+                if P.modeData.line>=lineTarget then
+                    P:finish('AC')
+                end
+                if P.modeData.line>bgmTransBegin and P.modeData.line<bgmTransFinish+4 and P.isMain then
+                    BGM.set(bgmList['echo'].add,'volume',math.min((P.modeData.line-bgmTransBegin)/(bgmTransFinish-bgmTransBegin),1),2.6)
+                end
+            end,
+            drawInField=function(P)
+                gc.setColor(1,1,1,.26)
+                gc.rectangle('fill',0,(P.modeData.line-lineTarget)*40-2,P.settings.fieldW*40,4)
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(lineTarget-P.modeData.line,-300,-55)
+            end,
+        },
+    }},
+}
