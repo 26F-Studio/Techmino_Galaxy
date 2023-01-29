68d6e0eea6e8db3482e486c28364e4097454a919
MrZ_26 <1046101471@qq.com>
2023-01-30 01:08:41 +0800

    添加迟钝20L模式

---
+++ assets/game/mode/mino/exterior/sprint_delay_20.lua
@@ -0,0 +1,37 @@
+local gc=love.graphics
+local lineTarget=20
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        BGM.play{
+            'race/accompany1',
+            'race/decoration',
+            'race/drum',
+        }
+    end,
+    settings={mino={
+        inputDelay=620,
+        event={
+            playerInit=function(P)
+                P.modeData.line=0
+            end,
+            afterClear=function(P,movement)
+                P.modeData.line=math.min(P.modeData.line+#movement.clear,lineTarget)
+                if P.modeData.line>=lineTarget then
+                    P:finish('AC')
+                end
+            end,
+            drawInField=function(P)
+                gc.setColor(1,1,1,.26)
+                gc.rectangle('fill',0,(P.modeData.line-lineTarget)*40-2,P.settings.fieldW*40,4)
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(lineTarget-P.modeData.line,-300,-55)
+            end,
+        },
+    }},
+}
--- assets/language/lang_en.lua
+++ assets/language/lang_en.lua
@@ -215,6 +215,7 @@ return {
         sprint_penta_40=       {"Sprint 40 Penta","Clear 40 lines with pentominoes"},
         sprint_sym_40=         {"Sprint 40 Sym","Clear 40 lines with symmetry field"},
         sprint_mph_40=         {"Sprint 40 MPH","Clear 40 lines without sequence information"},
+        sprint_delay_20=       {"Sprint 20 Delay","Clear 20 lines with massive input delay"},
         sprint_wind_40=        {"Sprint 40 Wind","Clear 40 lines under strong wind"},
         sprint_lock_20=        {"Sprint 20 Lock","Clear 20 lines without rotating piece"},
         sprint_fix_20=         {"Sprint 20 Fix","Clear 20 lines without moving piece"},
