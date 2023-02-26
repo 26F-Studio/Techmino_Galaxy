598faf96b76c43d4527edd99f9e1b415d84f53c2
MrZ_26 <1046101471@qq.com>
2023-02-27 01:03:57 +0800

    限制arr和dasHalt设置不能小于das

--- assets/scene/setting_in.lua
+++ assets/scene/setting_in.lua
@@ -42,8 +42,8 @@ scene.widgetList={
     WIDGET.new{type='slider',   pos={1,.5},x=-550, y=-210,w=400, fontSize=40,text=LANG'setting_sfx',     widthLimit=260, disp=TABLE.func_getVal(SETTINGS.system,'sfxVol'),  code=TABLE.func_setVal(SETTINGS.system,'sfxVol')},
     WIDGET.new{type='slider',   pos={1,.5},x=-550, y=-130,w=400, fontSize=40,text=LANG'setting_vib',     widthLimit=260, disp=TABLE.func_getVal(SETTINGS.system,'vibVol'),  code=TABLE.func_setVal(SETTINGS.system,'vibVol')},
 
-    WIDGET.new{type='slider',   name='das',    pos={1,.5},x=-800, y=50, w=650, text=LANG'setting_das',    widthLimit=260,axis={100,260,10},disp=TABLE.func_getVal(SETTINGS.game_mino,'das'),        valueShow=sliderShow_time, code=TABLE.func_setVal(SETTINGS.game_mino,'das')},
-    WIDGET.new{type='slider',   name='arr',    pos={1,.5},x=-800, y=150,w=650, text=LANG'setting_arr',    widthLimit=260,axis={20,120,10}, disp=TABLE.func_getVal(SETTINGS.game_mino,'arr'),        valueShow=sliderShow_time, code=TABLE.func_setVal(SETTINGS.game_mino,'arr')},
+    WIDGET.new{type='slider',   name='das',    pos={1,.5},x=-800, y=50, w=650, text=LANG'setting_das',    widthLimit=260,axis={100,260,10},disp=TABLE.func_getVal(SETTINGS.game_mino,'das'),        valueShow=sliderShow_time, code=function(v) SETTINGS.game_mino.das=v; SETTINGS.game_mino.arr=math.min(SETTINGS.game_mino.arr,SETTINGS.game_mino.das) end},
+    WIDGET.new{type='slider',   name='arr',    pos={1,.5},x=-800, y=150,w=650, text=LANG'setting_arr',    widthLimit=260,axis={20,120,10}, disp=TABLE.func_getVal(SETTINGS.game_mino,'arr'),        valueShow=sliderShow_time, code=function(v) SETTINGS.game_mino.arr=v; SETTINGS.game_mino.das=math.max(SETTINGS.game_mino.das,SETTINGS.game_mino.arr) end},
     WIDGET.new{type='slider',   name='sdarr',  pos={1,.5},x=-800, y=250,w=650, text=LANG'setting_sdarr',  widthLimit=260,axis={20,100,10}, disp=TABLE.func_getVal(SETTINGS.game_mino,'sdarr'),      valueShow=sliderShow_time, code=TABLE.func_setVal(SETTINGS.game_mino,'sdarr')},
 }
 return scene
