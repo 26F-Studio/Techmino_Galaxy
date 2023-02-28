9158b077e2484334e4906b318fbf98d2f83c304a
MrZ_26 <1046101471@qq.com>
2023-02-28 14:09:24 +0800

    增加对称40L模式

---
+++ assets/game/mode/mino/exterior/sprint_sym_40.lua
@@ -0,0 +1,59 @@
+local gc=love.graphics
+local lineTarget=40
+local bgmTransBegin,bgmTransFinish=10,30
+
+return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('race','base')
+    end,
+    settings={mino={
+        event={
+            playerInit=function(P)
+                P.modeData.line=0
+                P.modeData._coords={}
+            end,
+            afterDrop=function(P)
+                local CB=P.hand.matrix
+                for y=1,#CB do for x=1,#CB[1] do if CB[y][x] then
+                    table.insert(P.modeData._coords,{P.handX+x-1,P.handY+y-1})
+                end end end
+            end,
+            afterLock=function(P)
+                local id=nil
+                for _,coord in next,P.modeData._coords do
+                    local C=P.field:getCell((P.settings.fieldW+1)-coord[1],coord[2])
+                    C=C and C.id or false
+                    if id==nil then
+                        id=C
+                    elseif id~=C then
+                        for _,coord in next,P.modeData._coords do
+                            P.field:setCell(false,coord[1],coord[2])
+                        end
+                        break
+                    end
+                end
+                TABLE.cut(P.modeData._coords)
+            end,
+            afterClear=function(P,movement)
+                P.modeData.line=math.min(P.modeData.line+#movement.clear,lineTarget)
+                if P.modeData.line>=lineTarget then
+                    P:finish('AC')
+                end
+                if P.modeData.line>bgmTransBegin and P.modeData.line<bgmTransFinish+4 and P.isMain then
+                    BGM.set(bgmList['race'].add,'volume',math.min((P.modeData.line-bgmTransBegin)/(bgmTransFinish-bgmTransBegin),1),2.6)
+                end
+            end,
+            drawInField=function(P)
+                gc.setColor(1,1,1,.26)
+                gc.rectangle('fill',0,(P.modeData.line-lineTarget)*40-2,P.settings.fieldW*40,4)
+            end,
+            drawOnPlayer=function(P)
+                gc.setColor(COLOR.L)
+                FONT.set(80)
+                GC.mStr(lineTarget-P.modeData.line,-300,-55)
+            end,
+        },
+    }},
+}
