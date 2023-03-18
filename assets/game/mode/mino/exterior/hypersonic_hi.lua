30ab56d958c86e31d5733eb7f60d3327118667f2
MrZ_26 <1046101471@qq.com>
2023-03-19 00:05:25 +0800

    超音速-高的主要玩法改为后500有垃圾行，锁延下限定在300ms

--- assets/game/mode/mino/exterior/hypersonic_hi.lua
+++ assets/game/mode/mino/exterior/hypersonic_hi.lua
@@ -1,16 +1,16 @@
 local gc=love.graphics
 local bgmTransBegin,bgmTransFinish=100,500
 local levels={
-    {lock=520,fresh=4400,spawn=150,clear=380,das=130,arr=29},
-    {lock=450,fresh=4200,spawn=140,clear=340,das=120,arr=28},
-    {lock=380,fresh=4000,spawn=130,clear=300,das=110,arr=27},
-    {lock=320,fresh=3800,spawn=120,clear=260,das=105,arr=26},
-    {lock=270,fresh=3600,spawn=110,clear=220,das=100,arr=25},
-    {lock=230,fresh=3400,spawn=100,clear=180,das=96, arr=24},
-    {lock=200,fresh=3200,spawn=95, clear=160,das=92, arr=23},
-    {lock=180,fresh=3000,spawn=90, clear=140,das=88, arr=22},
-    {lock=170,fresh=2800,spawn=85, clear=120,das=84, arr=21},
-    {lock=160,fresh=2600,spawn=80, clear=100,das=80, arr=20},
+    {lock=450,fresh=4400,spawn=150,clear=380,das=130,arr=29,bumpInterval=false},
+    {lock=400,fresh=4200,spawn=140,clear=340,das=120,arr=28,bumpInterval=false},
+    {lock=360,fresh=4000,spawn=130,clear=300,das=110,arr=27,bumpInterval=false},
+    {lock=330,fresh=3800,spawn=120,clear=260,das=105,arr=26,bumpInterval=false},
+    {lock=300,fresh=3600,spawn=110,clear=220,das=100,arr=25,bumpInterval=false},
+    {lock=300,fresh=3400,spawn=100,clear=180,das=96, arr=24,bumpInterval=10000},
+    {lock=300,fresh=3200,spawn=95, clear=160,das=92, arr=23,bumpInterval=8000},
+    {lock=300,fresh=3000,spawn=90, clear=140,das=88, arr=22,bumpInterval=6000},
+    {lock=300,fresh=2800,spawn=85, clear=120,das=84, arr=21,bumpInterval=4000},
+    {lock=300,fresh=2600,spawn=80, clear=100,das=80, arr=20,bumpInterval=3000},
 }
 
 return {
@@ -28,6 +28,9 @@ return {
                 P.modeData.storedDas=P.settings.das
                 P.modeData.storedArr=P.settings.arr
 
+                P.modeData.bumpTimer0=false
+                P.modeData.bumpTimer=false
+
                 P.settings.dropDelay=0
                 P.settings.das=math.max(P.modeData.storedDas,levels[1].das)
                 P.settings.arr=math.max(P.modeData.storedArr,levels[1].arr)
@@ -35,6 +38,35 @@ return {
                 P.settings.spawnDelay=levels[1].spawn
                 P.settings.clearDelay=levels[1].clear
                 P.settings.maxFreshTime=levels[1].fresh
+            end,
+            always=function(P)
+                if P.finished or not P.modeData.bumpTimer or P.field:getHeight()==0 then return end
+                P.modeData.bumpTimer=P.modeData.bumpTimer-1
+                if P.modeData.bumpTimer<=0 then
+                    local F=P.field
+                    P.modeData.bumpTimer=P.modeData.bumpTimer0
+
+                    local firstLine=F._matrix[1]
+                    local newLine={}
+                    for i=1,#firstLine do
+                        newLine[i]=firstLine[i] and {color=firstLine[i].color,conn={}} or false
+                    end
+                    table.insert(F._matrix,1,newLine)
+                    for i=1,#firstLine do
+                        local cells=P:getConnectedCells(i,2)
+                        for j=1,#cells do
+                            local c=F:getCell(cells[j][1],cells[j][2])
+                            c.conn[newLine[i]]=true
+                            newLine[i].conn[c]=true
+                        end
+                    end
+                    if P.hand then
+                        P.handY=P.handY+1
+                        P.minY=P.minY+1
+                        P:freshGhost()
+                    end
+                    P:playSound('move_failed')
+                end
             end,
             afterSpawn=function(P)
                 local md=P.modeData
@@ -60,12 +92,18 @@ return {
                         P:playSound('reach')
                         md.level=md.level+1
                         md.target=100*md.level
+
                         P.settings.das=math.max(P.modeData.storedDas,levels[md.level].das)
                         P.settings.arr=math.max(P.modeData.storedArr,levels[md.level].arr)
                         P.settings.lockDelay=levels[md.level].lock
                         P.settings.spawnDelay=levels[md.level].spawn
                         P.settings.clearDelay=levels[md.level].clear
                         P.settings.maxFreshTime=levels[md.level].fresh
+
+                        md.bumpTimer0=levels[md.level].bumpInterval
+                        if md.bumpTimer0 then
+                            md.bumpTimer=md.bumpTimer0
+                        end
                     else
                         P:finish('AC')
                     end
