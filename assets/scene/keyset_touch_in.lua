dd8592d76c873324251928804547aeb6104e4e52
MrZ_26 <1046101471@qq.com>
2023-04-11 17:15:22 +0800

    再微调虚拟按键设置界面

--- assets/scene/keyset_touch_in.lua
+++ assets/scene/keyset_touch_in.lua
@@ -60,23 +60,23 @@ function scene.mouseUp(x,y,k) if k==1 then scene.touchUp(x,y,1) end end
 function scene.draw() VCTRL.draw(true) end
 
 scene.widgetList={
-    WIDGET.new{type='button',pos={0,.5},  x=210,y=-360,w=200,h=80,lineWidth=4,cornerR=0,sound='button_back',fontSize=60,text=CHAR.icon.back,code=WIDGET.c_backScn('none')},
+    WIDGET.new{type='button',                pos={0,.5}, x=210, y=-360,w=200,h=80,sound='button_back',fontSize=60,text=CHAR.icon.back,code=WIDGET.c_backScn('none'),lineWidth=4,cornerR=0},
 
-    WIDGET.new{type='button',pos={0,.5},  x=520,y=-360,w=200,h=80,lineWidth=4,cornerR=0,fontSize=25,text=LANG'stick2_switch',code=WIDGET.c_pressKey('q')},
-    WIDGET.new{type='button',pos={0,.5},  x=520,y=-270,w=200,h=80,lineWidth=4,cornerR=0,fontSize=25,text=LANG'stick4_switch',code=WIDGET.c_pressKey('w')},
+    WIDGET.new{type='button',                pos={.5,.5},x=-330,y=-360,w=200,h=80,fontSize=25,text=LANG'stick2_switch',code=WIDGET.c_pressKey('q'),lineWidth=4,cornerR=0},
+    WIDGET.new{type='button',                pos={.5,.5},x=-330,y=-270,w=200,h=80,fontSize=25,text=LANG'stick4_switch',code=WIDGET.c_pressKey('w'),lineWidth=4,cornerR=0},
 
-    WIDGET.new{type='button',x=940, y=150,w=100,lineWidth=4,cornerR=0,fontSize=80,text='-',code=WIDGET.c_pressKey('-')},
-    WIDGET.new{type='button',x=1460,y=150,w=100,lineWidth=4,cornerR=0,fontSize=80,text='+',code=WIDGET.c_pressKey('+')},
-    WIDGET.new{type='text',  x=1200,y=150,text=LANG'setting_touch_button',fontSize=40,widthLimit=400},
+    WIDGET.new{type='button',                pos={.5,.5},x=40,  y=-350,w=100,fontSize=80,text='-',code=WIDGET.c_pressKey('-'),lineWidth=4,cornerR=0},
+    WIDGET.new{type='button',                pos={.5,.5},x=560, y=-350,w=100,fontSize=80,text='+',code=WIDGET.c_pressKey('+'),lineWidth=4,cornerR=0},
+    WIDGET.new{type='text',                  pos={.5,.5},x=300, y=-350,fontSize=40,widthLimit=400,text=LANG'setting_touch_button',},
 
-    WIDGET.new{type='slider',name='button1', x=900, y=280,w=600,axis={60,260,20},  text=LANG'setting_touch_buttonSize',  widthLimit=200, disp=function() return VCTRL.focus and VCTRL.focus.type=='button' and VCTRL.focus.r end,code=function(v) if VCTRL.focus then VCTRL.focus.r=v end end},
-    WIDGET.new{type='button',name='button2', x=1200,y=400,w=360,h=100,fontSize=40,        text=LANG'settinh_touch_buttonShape', code=WIDGET.c_pressKey('0')},
+    WIDGET.new{type='slider',name='button1', pos={.5,.5},x=0,   y=-220,w=600,axis={60,260,20},  text=LANG'setting_touch_buttonSize',  widthLimit=200, disp=function() return VCTRL.focus and VCTRL.focus.type=='button' and VCTRL.focus.r end,code=function(v) if VCTRL.focus then VCTRL.focus.r=v end end},
+    WIDGET.new{type='button',name='button2', pos={.5,.5},x=300, y=-100,w=360,h=100,fontSize=40, text=LANG'settinh_touch_buttonShape', code=WIDGET.c_pressKey('0'),lineWidth=4,cornerR=0},
 
-    WIDGET.new{type='slider',name='stick2_1',x=900, y=280,w=600,axis={200,500,25}, text=LANG'setting_touch_stickSize',   widthLimit=200, disp=function() return VCTRL.focus.len  end,code=function(v) VCTRL.focus.len=v    end},
-    WIDGET.new{type='slider',name='stick2_2',x=900, y=380,w=600,axis={100,200,10}, text=LANG'setting_touch_ballSize',    widthLimit=200, disp=function() return VCTRL.focus.h    end,code=function(v) VCTRL.focus.h=v      end},
+    WIDGET.new{type='slider',name='stick2_1',pos={.5,.5},x=0,   y=-220,w=600,axis={200,500,25}, text=LANG'setting_touch_stickSize',   widthLimit=200, disp=function() return VCTRL.focus.len  end,code=function(v) VCTRL.focus.len=v    end},
+    WIDGET.new{type='slider',name='stick2_2',pos={.5,.5},x=0,   y=-120,w=600,axis={100,200,10}, text=LANG'setting_touch_ballSize',    widthLimit=200, disp=function() return VCTRL.focus.h    end,code=function(v) VCTRL.focus.h=v      end},
 
-    WIDGET.new{type='slider',name='stick4_1',x=900, y=280,w=600,axis={100,250,25}, text=LANG'setting_touch_stickSize',   widthLimit=200, disp=function() return VCTRL.focus.r    end,code=function(v) VCTRL.focus.r=v      end},
-    WIDGET.new{type='slider',name='stick4_2',x=900, y=380,w=600,axis={.1,.8,.1},   text=LANG'setting_touch_ballSize',    widthLimit=200, disp=function() return VCTRL.focus.ball end,code=function(v) VCTRL.focus.stickR=v end},
+    WIDGET.new{type='slider',name='stick4_1',pos={.5,.5},x=0,   y=-220,w=600,axis={100,250,25}, text=LANG'setting_touch_stickSize',   widthLimit=200, disp=function() return VCTRL.focus.r    end,code=function(v) VCTRL.focus.r=v      end},
+    WIDGET.new{type='slider',name='stick4_2',pos={.5,.5},x=0,   y=-120,w=600,axis={.1,.8,.1},   text=LANG'setting_touch_ballSize',    widthLimit=200, disp=function() return VCTRL.focus.ball end,code=function(v) VCTRL.focus.stickR=v end},
 }
 
 return scene
