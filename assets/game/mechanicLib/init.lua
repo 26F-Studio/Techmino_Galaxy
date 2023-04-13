53f1f83f305c2421f4f7dafcfa959f5d527daba3
MrZ_26 <1046101471@qq.com>
2023-04-14 03:23:32 +0800

    把几个生存模式放进机制库

--- assets/game/mechanicLib/init.lua
+++ assets/game/mechanicLib/init.lua
@@ -1,7 +1,13 @@
 return {
     mino={
+        -- Basic
         statistics=require'assets.game.mechanicLib.mino.statistics',
+
+        -- Mode
         sprint=require'assets.game.mechanicLib.mino.sprint',
+        survivor=require'assets.game.mechanicLib.mino.survivor',
+
+        -- Special
         stack=require'assets.game.mechanicLib.mino.stack',
     },
     puyo={},
---
+++ assets/game/mechanicLib/mino/survivor.lua
@@ -0,0 +1,128 @@
+local floor,ceil=math.floor,math.ceil
+local max,min=math.max,math.min
+local gc=love.graphics
+
+local survivor={}
+
+function survivor.event_playerInit(P)
+    local md=P.modeData
+    md.waveTimer0=1
+    md.waveTimer=0
+    md.wave=0
+end
+
+function survivor.event_drawOnPlayer(P)
+    P:drawInfoPanel(-380,-60,160,120)
+    FONT.set(80) GC.mStr(P.modeData.wave,-300,-70)
+    FONT.set(30) GC.mStr("Waves",-300,15)
+
+    local cd=P.modeData.waveTimer/P.modeData.waveTimer0
+    gc.setLineWidth(10)
+    gc.setColor(COLOR.DL)
+    gc.arc('line','open',-300,130,40,-MATH.pi*.5,cd*MATH.tau-MATH.pi*.5)
+    gc.setLineWidth(3)
+    gc.setColor(COLOR.lD)
+    gc.circle('line',-300,130,32)
+    gc.circle('line',-300,130,48)
+end
+
+function survivor.event_always_b2b(P)
+    if not P.timing then return end
+    local md=P.modeData
+    if md.waveTimer>0 then
+        md.waveTimer=min(md.waveTimer-1,(P.field:getHeight()+3*P.garbageSum)*260)
+    end
+    if md.waveTimer==0 and P.garbageSum<=max(20-P.field:getHeight(),12) then
+        md.wave=md.wave+1
+
+        local wave=md.wave
+        md.waveTimer0=math.floor(
+            wave<=40 and MATH.interpolate(wave,0,6000,40,4000) or
+            wave<=80 and MATH.interpolate(wave,40,4000,80,1500) or
+            max(MATH.interpolate(wave,80,1500,150,800),800)
+        )
+        md.waveTimer=md.waveTimer0
+        GAME.send(nil,GAME.initAtk{
+            target=GAME.mainID,
+            power=4+P:random(0,MATH.clamp(math.floor(wave/30-P.field:getHeight()/10),0,3)),
+            mode=0,
+            time=
+                (wave<50 and 100000/(100+2*wave)+500 or 1000)+
+                MATH.clamp(400-wave,150,350)*max(P.field:getHeight()+P.garbageSum-8,0),
+            fatal=math.floor(min(30+wave/2,50)),
+            -- speed=?,
+        })
+    end
+end
+function survivor.event_always_cheese(P)
+    if not P.timing then return end
+    local md=P.modeData
+    if md.waveTimer>0 then
+        md.waveTimer=min(md.waveTimer-1,(P.field:getHeight()+3*P.garbageSum)*260)
+    end
+    if md.waveTimer==0 and P.garbageSum<=max(15-P.field:getHeight()/2,8) then
+        md.wave=md.wave+1
+
+        local wave=md.wave
+        md.waveTimer0=math.floor(
+            wave<=80 and MATH.interpolate(wave,0,3000,80,2000) or
+            wave<=150 and MATH.interpolate(wave,80,2000,150,2500) or
+            math.max(2000-(wave-150)*10,1000)
+        )
+        md.waveTimer=md.waveTimer0
+        GAME.send(nil,GAME.initAtk{
+            target=GAME.mainID,
+            power=P:random(0,10)+P:random(-5,5)>=P.field:getHeight() and 2 or 1,
+            defendRate=wave<60 and 2 or 3,
+            mode=0,
+            time=wave<50 and 100000/(50+wave)-1000 or 0,
+            fatal=MATH.clamp(
+                (
+                    wave<100 and MATH.interpolate(wave,0,20,100,60) or
+                    wave<200 and MATH.interpolate(wave,100,60,200,100) or
+                    100
+                )+math.floor((P:random()*2-1)*min(wave,100)/5),
+                0,100
+            ),
+            -- speed=?,
+        })
+    end
+end
+function survivor.event_always_spike(P)
+    if not P.timing then return end
+    local md=P.modeData
+    if md.waveTimer>0 then
+        md.waveTimer=min(md.waveTimer-1,P.garbageSum*620)
+    end
+    if md.waveTimer==0 and P.garbageSum<=10 then
+        md.wave=md.wave+1
+
+        local wave=md.wave
+        md.waveTimer0=floor(
+            wave<=10 and MATH.interpolate(wave,0,20000,10,15000) or
+            wave<=30 and MATH.interpolate(wave,10,15000,30,12000) or
+            max(MATH.interpolate(wave,30,12000,60,10000),10000)
+        )
+        md.waveTimer=md.waveTimer0
+        for i=1,3 do
+            GAME.send(nil,GAME.initAtk{
+                target=GAME.mainID,
+                power=
+                    2+2*i+ -- Basic Attack (4+6+8)
+                    P:random(
+                        MATH.clamp(ceil(wave/20-P.field:getHeight()/15),0,2), -- Random lower bound (from 0 to 2 on wave 40, -1 if field reach 15)
+                        min(floor(wave/10),4) -- Random Upper bound (from 0 to 4 on wave 40)
+                    ),
+                mode=0,
+                time=
+                    max(6000-50*wave,4000)+ -- Basic delay (from 6s to 4s on wave 40)
+                    (i-1)*max(3000-wave*30,1500), -- Split time (from 3s to 1.5s on wave 50)
+                fatal=floor(min(30+wave/4,50)),-- Fatal (from 30 to 50 on wave 80)
+                -- speed=?,
+            })
+        end
+
+    end
+end
+
+return survivor
