1e0e0dd00547150532a9371bce32d48fa3be365f
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-04-15 21:30:07 +0700

    Get new Zictionary UI + first commit of it 15th/April/2023

---
+++ assets/language/dict_vi.lua
@@ -0,0 +1,10 @@
+return {
+    aboutDict={
+        title="Thông tin về Zictionary",
+        content="Zictionary sẽ dạy cho bạn mọi thứ về xếp gạch",
+    },
+    aboutDict_hidden={
+        title="Cái này à?",
+        content="Nó sẽ dạy cho bạn mọi thứ về xếp gạch.\n\nMà ngon lành rồi,\nbạn đã biết cách mở từ điển bằng cách nhấn phím tắt rồi đấy!",
+    },
+}
