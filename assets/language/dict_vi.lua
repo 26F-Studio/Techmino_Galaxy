464af229a3e69754e3ae97bee5106d53b72d3bd7
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-04-16 11:19:11 +0700

    small note in dictionary

--- assets/language/dict_vi.lua
+++ assets/language/dict_vi.lua
@@ -8,3 +8,5 @@ return {
         content="Nó sẽ dạy cho bạn mọi thứ về xếp gạch.\n\nMà ngon lành rồi,\nbạn đã biết cách mở từ điển bằng cách nhấn phím tắt rồi đấy!",
     },
 }
+
+-- Always use font size which can be divided by 5 (ex: 5, 10, 15, 20, 25,...)
