e012345e012f9ff7f0e564a878b6ae0746298448
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-04-17 14:58:06 +0700

    Update dictionary

--- assets/language/dict_vi.lua
+++ assets/language/dict_vi.lua
@@ -7,6 +7,43 @@ return {
         title="Cái gì đây?",
         content="Nó sẽ dạy cho bạn mọi thứ về xếp gạch.\n\nMà ngon lành rồi,\nbạn đã biết cách mở từ điển bằng cách nhấn phím tắt rồi đấy!",
     },
+    setting_out={
+        title="Cài đặt",
+        content="Đây là trang Cài đặt.\nBạn có thể chỉnh một đống thứ ngay tại đây. ",
+    },
+    noobGuide={
+        title="Welcome Newcomers",
+        content="These are sets of missions that you need to accomplish. When there is order within each set, we recommend you to do all three sets of tasks simultaneously instead of one by one.\n\nA. Stacking\n\tA1. Think twice before you place your block. If your first decision doesn’t seem to fit well, think again.\n\tA2. Keep your terrain flat since it allows more possibilities for different blocks.\n\tA3. When hold allowed, thinking more about sequences to arrange the pieces in your hand, the held one, plus the previews. In order to have flat terrain for longer time.\n\nB. Efficiency & Speed\n\tB1. Don’t count on ghost pieces. Think, with your brain, where this piece would land and what keys you should press. Do it only when you are ready.\n\tB2. Use both of the rotation keys. Don’t just use one and press it many times when you can press the other one just once.\n\tB3. Don’t worry too much about your speed when you start learning Finesse. Keep your move accurate, and then it is not hard to improve your speed once you have mastered it.\n\nC. Stacking\n\tC1. Finish 40L without topping out.\n\tC2. Finish 40L with no Hold without topping out.\n\tC3. Finish 40L with all Techrashes.\n\tC4. Finish 40L with all Techrashes and no Hold.\n\nSet C is more flexible, and you can adjust the difficulties based on your own conditions (like what does ” means to you).\n\nWhen you finish all the tasks in set C, keep practicing A1. This is the fundamental skill in any Tetris game, and you can master practically any mode when you can just scan through the Next sequence easily.",
+        contentSize=20,
+    },
+    noobGuide={
+        title="Chào những người bạn mới", 
+        contentSize=20,
+        content=[===[
+Đây là một vài nhóm nhiệm vụ mà bạn nên làm mỗi khi chơi. Mặc dù từng nhóm đều có trật tự cụ thể, chúng tôi khuyên bạn nên làm cả ba nhóm cùng lúc thay vì làm từng cái một.
+
+A1. Stacking (Xếp gạch)
+    A1. Suy nghĩ kỹ trước khi đặt gạch. Nếu cảm thấy không hợp lí, suy nghĩ thêm lần nữa.
+    A2. Hãy giữ cho địa hình gạch bằng phẳng để bạn có thể ra quyết định đặt gạch dễ dàng hơn.
+    A3. Nếu bạn có thể sử dụng HOLD, hãy suy nghĩ về dãy gạch bạn đang có và sắp xếp lại chúng: gạch đang giữ và gạch sẽ rơi. Vì bạn vẫn phải giữ địa hình gạch bằng phẳng trong thời gian dài
+
+B. Efficiency & Speed (Hiệu quả & Tốc độ)
+    B1. Đừng có dựa dẫm vào mấy cái gạch ma! Hãy suy nghĩ, dùng cái đầu, tính xem bạn sẽ cho gạch rơi ở đâu và bạn cần nhấn những phím nào. Chỉ nhấn phím khi bạn đã sẵn sàng.
+    B2. Hãy tập dùng cả hai phím xoay. Đừng có nhấn một phím xoay nào đó quá nhiều lần trong khi bạn có thể nhấn phím kia chỉ 1 lần mà thôi!
+        ]===],
+    },
+    ['20g']={
+        title="20G",
+        content="The fastest falling speed of modern Tetris. In 20G modes, pieces appear instantly on the bottom of the field without the actual process of falling. This sometimes also limits a piece’s sideways movements, as it is not always possible to make a piece climb over a bump or out of a well in 20G. You can learn more at the unit “G” at the “falling speed” entry.",
+    },
+    hypertap={
+        title="Hypertap",
+        content="Refers to the technique of vibrating your finger on the controller to achieve faster sideways movement speed than holding it.\nIt is most commonly used on classic Tetris where DAS is relatively slow. In most cases, you do not need to hypertap in modern Tetris games because their DAS is usually fast enough.",
+    },
+    ['26f_studio']={
+        title="26F Studio",
+        content="A group",
+    },
 }
 
 -- Always use font size which can be divided by 5 (ex: 5, 10, 15, 20, 25,...)
