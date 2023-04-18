1bee856454e501fcb6bd47dddcc15fe29add5b02
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-04-18 12:12:37 +0700

    Update dicttionary
    Update "Welcome newcomers" entry

--- assets/language/dict_vi.lua
+++ assets/language/dict_vi.lua
@@ -12,24 +12,35 @@ return {
         content="Đây là trang Cài đặt.\nBạn có thể chỉnh một đống thứ ngay tại đây. ",
     },
     noobGuide={
-        title="Welcome Newcomers",
-        content="These are sets of missions that you need to accomplish. When there is order within each set, we recommend you to do all three sets of tasks simultaneously instead of one by one.\n\nA. Stacking\n\tA1. Think twice before you place your block. If your first decision doesn’t seem to fit well, think again.\n\tA2. Keep your terrain flat since it allows more possibilities for different blocks.\n\tA3. When hold allowed, thinking more about sequences to arrange the pieces in your hand, the held one, plus the previews. In order to have flat terrain for longer time.\n\nB. Efficiency & Speed\n\tB1. Don’t count on ghost pieces. Think, with your brain, where this piece would land and what keys you should press. Do it only when you are ready.\n\tB2. Use both of the rotation keys. Don’t just use one and press it many times when you can press the other one just once.\n\tB3. Don’t worry too much about your speed when you start learning Finesse. Keep your move accurate, and then it is not hard to improve your speed once you have mastered it.\n\nC. Stacking\n\tC1. Finish 40L without topping out.\n\tC2. Finish 40L with no Hold without topping out.\n\tC3. Finish 40L with all Techrashes.\n\tC4. Finish 40L with all Techrashes and no Hold.\n\nSet C is more flexible, and you can adjust the difficulties based on your own conditions (like what does ” means to you).\n\nWhen you finish all the tasks in set C, keep practicing A1. This is the fundamental skill in any Tetris game, and you can master practically any mode when you can just scan through the Next sequence easily.",
-        contentSize=20,
-    },
-    noobGuide={
-        title="Chào những người bạn mới", 
+        title="Chào bạn mới", 
         contentSize=20,
         content=[===[
-Đây là một vài nhóm nhiệm vụ mà bạn nên làm mỗi khi chơi. Mặc dù từng nhóm đều có trật tự cụ thể, chúng tôi khuyên bạn nên làm cả ba nhóm cùng lúc thay vì làm từng cái một.
+Bạn mới tập chơi xếp gạch à? Nếu vậy thì…
+Đây là một vài thứ mà bạn nên làm mỗi khi chơi, chúng được sắp xếp thành một nhóm. Tuy xếp thành nhóm, chúng tôi khuyên bạn nên làm cả ba nhóm cùng lúc thay vì làm từng cái một.
 
-A1. Stacking (Xếp gạch)
-    A1. Suy nghĩ kỹ trước khi đặt gạch. Nếu cảm thấy không hợp lí, suy nghĩ thêm lần nữa.
-    A2. Hãy giữ cho địa hình gạch bằng phẳng để bạn có thể ra quyết định đặt gạch dễ dàng hơn.
-    A3. Nếu bạn có thể sử dụng HOLD, hãy suy nghĩ về dãy gạch bạn đang có và sắp xếp lại chúng: gạch đang giữ và gạch sẽ rơi. Vì bạn vẫn phải giữ địa hình gạch bằng phẳng trong thời gian dài
+A. Stacking (Xếp gạch)
+    A1. Suy nghĩ kỹ trước khi đặt gạch. Nếu cảm thấy chưa vừa ý, hãy suy nghĩ thêm lần nữa.
+    A2. Hãy xếp gạch càng phẳng càng tốt để bạn có thể ra quyết định đặt gạch dễ dàng hơn.
+    A3. Vì bạn sẽ phải xếp gạch càng phẳng càng tốt trong thời gian dài, bạn nên lập kế hoạch xếp như thế nào dựa trên NEXT để giữ được thể đẹp, và nên tận dụng HOLD nếu có thể
 
 B. Efficiency & Speed (Hiệu quả & Tốc độ)
-    B1. Đừng có dựa dẫm vào mấy cái gạch ma! Hãy suy nghĩ, dùng cái đầu, tính xem bạn sẽ cho gạch rơi ở đâu và bạn cần nhấn những phím nào. Chỉ nhấn phím khi bạn đã sẵn sàng.
-    B2. Hãy tập dùng cả hai phím xoay. Đừng có nhấn một phím xoay nào đó quá nhiều lần trong khi bạn có thể nhấn phím kia chỉ 1 lần mà thôi!
+    B1. Trước mỗi lần đặt gạch, hãy suy nghĩ xem bạn sẽ đặt gạch ở đâu? Bấm những phím nào để gạch tới chỗ đó và đứng đúng tư thế? Thay vì dựa dẫm vào gạch ma quá nhiều
+    B2. Bạn vẫn còn triple xoay á? Thay vì chỉ xài một phím, hãy thử (tập) dùng (các) phím xoay còn lại đi. Nhiều lúc bạn chỉ cần nhấn 1 phím xoay duy nhất để xoay gạch thôi (nếu game có hỗ trợ xoay 180°, nếu không thì tối đa là 2 phím)
+    B3. Đừng lo lắng vụ tốc độ khi bạn mới tập chơi Finesse¹, ai mới tập chơi đều gặp hiện tượng tốc độ cực chậm (như mạng 2G). Nhừng đừng lo lắng, một khi bạn đã quen tay thì bạn có thể cải thiện tốc độ chơi của mình — việc này không quá khó đâu!
+    
+C. Stacking (Xếp gạch)
+    C1. Hoàn thành chế độ "40 hàng" mà không được thua.
+    C2. Hoàn thành chế độ "40 hàng" mà không dùng HOLD cũng như không được thua.
+    C3. Hoàn thành chế độ "40 hàng" mà chỉ được làm Techrash.
+    C4. Hoàn thành mà chỉ được làm Techrash và không được dùng HOLD
+    
+Nhóm C rất linh động, bạn có thể điều chỉnh độ khó dựa trên tình hình/điều kiện của bạn (giống như cái ký tự ” này có ý nghĩa gì đối với bạn)
+
+Sau khi bạn hoàn thành hết nhóm C, hãy tiếp tục luyện tập phần A1, đây là một kỹ năng tối quan trọng trong bất kỳ tựa game xếp gạch nào; và bạn sẽ có thể dần dần làm chủ bất kỳ chế độ nào khi mà bạn chỉ cần nhìn lướt qua NEXT
+    
+Chú thích:
+    1:  Finesse (tạm dịch: "sự khéo léo"): kỹ năng xếp gạch chính xác trong khi giữ được tốc độ chơi đồng đều trong suốt thời gian chơi. 
+          Để biết rõ hơn, hãy tra mục "Finesse", 
         ]===],
     },
     ['20g']={
