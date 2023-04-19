77d61952fe1f0234c6831553b5c0887f9ac8048d
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-04-19 09:10:45 +0700

    Cập nhật phần từ điển

--- assets/language/dict_vi.lua
+++ assets/language/dict_vi.lua
@@ -21,35 +21,36 @@ Bạn mới tập chơi xếp gạch à? Nếu vậy thì…
 A. Stacking (Xếp gạch)
     A1. Suy nghĩ kỹ trước khi đặt gạch. Nếu cảm thấy chưa vừa ý, hãy suy nghĩ thêm lần nữa.
     A2. Hãy xếp gạch càng phẳng càng tốt để bạn có thể ra quyết định đặt gạch dễ dàng hơn.
-    A3. Vì bạn sẽ phải xếp gạch càng phẳng càng tốt trong thời gian dài, bạn nên lập kế hoạch xếp như thế nào dựa trên NEXT để giữ được thể đẹp, và nên tận dụng HOLD nếu có thể
+    A3. Hãy lập kế hoạch xếp như thế nào dựa trên NEXT để giữ được thể đẹp, và hãy tận dụng HOLD nếu có thể
 
 B. Efficiency & Speed (Hiệu quả & Tốc độ)
     B1. Trước mỗi lần đặt gạch, hãy suy nghĩ xem bạn sẽ đặt gạch ở đâu? Bấm những phím nào để gạch tới chỗ đó và đứng đúng tư thế? Thay vì dựa dẫm vào gạch ma quá nhiều
-    B2. Bạn vẫn còn triple xoay á? Thay vì chỉ xài một phím, hãy thử (tập) dùng (các) phím xoay còn lại đi. Nhiều lúc bạn chỉ cần nhấn 1 phím xoay duy nhất để xoay gạch thôi (nếu game có hỗ trợ xoay 180°, nếu không thì tối đa là 2 phím)
-    B3. Đừng lo lắng vụ tốc độ khi bạn mới tập chơi Finesse¹, ai mới tập chơi đều gặp hiện tượng tốc độ cực chậm (như mạng 2G). Nhừng đừng lo lắng, một khi bạn đã quen tay thì bạn có thể cải thiện tốc độ chơi của mình — việc này không quá khó đâu!
+    B2. Nên sử dụng 2 (hoặc 3, tùy game) phím xoay thay vì 1 phím xoay. Nhấn 1 phím xoay sẽ làm bạn chậm và làm cho bạn dễ rơi vào trạng thái bị động
+    B3. Đừng lo lắng vụ tốc độ khi bạn mới tập chơi Finesse, ai mới tập chơi đều gặp hiện tượng tốc độ cực chậm (như mạng 2G). Nhừng đừng lo lắng, một khi bạn đã quen tay thì bạn có thể cải thiện tốc độ chơi của mình — việc này không quá khó đâu!
     
 C. Stacking (Xếp gạch)
-    C1. Hoàn thành chế độ "40 hàng" mà không được thua.
-    C2. Hoàn thành chế độ "40 hàng" mà không dùng HOLD cũng như không được thua.
+    C1. Hoàn thành chế độ "40 hàng"
+    C2. Hoàn thành chế độ "40 hàng" mà không dùng HOLD
     C3. Hoàn thành chế độ "40 hàng" mà chỉ được làm Techrash.
-    C4. Hoàn thành mà chỉ được làm Techrash và không được dùng HOLD
+    C4. Hoàn thành chế độ "40 hàng" mà chỉ được làm Techrash và không được dùng HOLD
     
 Nhóm C rất linh động, bạn có thể điều chỉnh độ khó dựa trên tình hình/điều kiện của bạn (giống như cái ký tự ” này có ý nghĩa gì đối với bạn)
 
 Sau khi bạn hoàn thành hết nhóm C, hãy tiếp tục luyện tập phần A1, đây là một kỹ năng tối quan trọng trong bất kỳ tựa game xếp gạch nào; và bạn sẽ có thể dần dần làm chủ bất kỳ chế độ nào khi mà bạn chỉ cần nhìn lướt qua NEXT
-    
-Chú thích:
-    1:  Finesse (tạm dịch: "sự khéo léo"): kỹ năng xếp gạch chính xác trong khi giữ được tốc độ chơi đồng đều trong suốt thời gian chơi. 
-          Để biết rõ hơn, hãy tra mục "Finesse", 
         ]===],
     },
     ['20g']={
         title="20G",
-        content="The fastest falling speed of modern Tetris. In 20G modes, pieces appear instantly on the bottom of the field without the actual process of falling. This sometimes also limits a piece’s sideways movements, as it is not always possible to make a piece climb over a bump or out of a well in 20G. You can learn more at the unit “G” at the “falling speed” entry.",
+        content="Tốc độ nhanh nhất trong các trò Tetris hiện đại. Trong các chế độ xài tốc độ 20G, các miếng gạch thay vì rơi từ từ, nó sẽ xuất hiện ngay lập tức ở cuối bảng. Việc này đôi khi sẽ làm bạn không thể di chuyển được theo phương ngang như ý bạn muốn; bởi vì gạch đôi khi cũng không thể leo qua chỗ lồi lõm hoặc ra khỏi hố sâu. Bạn có thể tìm hiểu thêm về đơn vị \"G\" trong mục \"Tốc độ rơi\"",
     },
     hypertap={
         title="Hypertap",
-        content="Refers to the technique of vibrating your finger on the controller to achieve faster sideways movement speed than holding it.\nIt is most commonly used on classic Tetris where DAS is relatively slow. In most cases, you do not need to hypertap in modern Tetris games because their DAS is usually fast enough.",
+        title_full="Hypertap (Nhấn liên tục)",
+        content="Đề cập tới một kỹ năng là khi bạn \"rung\" tay liên tục để nhấn liên tục làm tốc độ di chuyển ngang\u{00B9} nhanh hơn"..[===[
+
+__________________________
+Chú thích:
+    1:  Di chuyển ngang: di chuyển sang trái và phải]===],
     },
     ['26f_studio']={
         title="26F Studio",
