a2bd8f5a53e75fe109e8647c3cea5147e2c2df01
MrZ_26 <1046101471@qq.com>
2023-04-20 16:38:15 +0800

    Format dict_vi

--- assets/language/dict_vi.lua
+++ assets/language/dict_vi.lua
@@ -1,95 +1,70 @@
-return {
-    aboutDict={
-        title="T.tin về Zictionary",
-        title_full="Thông tin về Zictionary",
-        content="Zictionary sẽ dạy cho bạn mọi thứ về xếp gạch",
-    },
-    aboutDict_hidden={
-        title="Cái gì đây?",
-        content="Nó sẽ dạy cho bạn mọi thứ về xếp gạch.\n\n... Khoan, chờ khoảng chừng là 2 giây!\nBạn vừa nhấn phím tắt à? Ngon lành cành đào rồi đây",
-    },
-    setting_out={
-        title="Cài đặt",
-        content="Đây là trang Cài đặt.\nBạn có thể chỉnh một đống thứ ở đây!",
-    },
+return [[
+# aboutDict
+@ title T.tin về Zictionary
+@ title_full Thông tin về Zictionary
+Zictionary sẽ dạy cho bạn mọi thứ về xếp gạch
 
---------------------------------------------------------------
+# aboutDict_hidden
+@ title Cái gì đây?
+Nó sẽ dạy cho bạn mọi thứ về xếp gạch.\n\n... Khoan, chờ khoảng chừng là 2 giây!\nBạn vừa nhấn phím tắt à? Ngon lành cành đào rồi đây
 
-    noobGuide={
-        title="Xin chào", 
-        contentSize=20,
-        content=[===[
+# setting_out
+@ title Cài đặt
+Đây là trang Cài đặt.\nBạn có thể chỉnh một đống thứ ở đây!
+
+# noobGuide
+@ title Xin chào
+@ contentSize 20
 Cảm ơn bạn đã tải và chơi Techmino! ... Hả? Đây là lần đầu bạn chơi game xếp gạch á? Nếu vậy thì...
 Đây là một vài thứ mà bạn nên làm mỗi khi chơi, chúng được sắp xếp thành một nhóm. Tuy xếp thành nhóm, bạn vẫn nên làm cả ba nhóm cùng lúc thay vì làm từng cái một.
-
+~~
 A. Stacking (Xếp gạch)
     A1. Suy nghĩ kỹ trước khi đặt gạch. Chưa vừa ý? Suy nghĩ thêm lần nữa.
     A2. Hãy xếp gạch càng phẳng càng tốt để bạn có thể ra quyết định đặt gạch dễ dàng hơn.
     A3. Lập chiến lược dựa trên NEXT, và nếu có thể thì hãy tận dụng HOLD để giữ được thể đẹp. 
-
+~~
 B. Efficiency & Speed (Hiệu quả & Tốc độ)
     B1. Trước mỗi lần đặt gạch, hãy suy nghĩ xem bạn sẽ đặt gạch ở đâu? Bấm những phím nào để gạch tới chỗ đó và đứng đúng tư thế? Thay vì dựa dẫm vào gạch ma quá nhiều
     B2. Nên sử dụng 2 (hoặc 3, tùy game) phím xoay thay vì nhấn 1 phím xoay liên tục trong thời gian dài. Bởi vì làm như vậy trong thời gian dài sẽ làm bạn chậm và làm cho bạn dễ rơi vào trạng thái bị động
     B3. Đừng lo lắng về tốc độ khi bạn mới tập chơi Finesse, đây là chuyện bình thường. Hơn nữa bạn có thể tập chơi nhanh hơn một khi bạn đã quen tay — việc này không khó đâu!
-    
+~~
 C. Practice (Luyện tập)¹
     C1. Hoàn thành chế độ "40 hàng"
     C2. Hoàn thành chế độ "40 hàng" mà không dùng HOLD
     C3. Hoàn thành chế độ "40 hàng" mà chỉ được làm Techrash.
     C4. Hoàn thành chế độ "40 hàng" mà chỉ được làm Techrash và không được dùng HOLD
-    
+~~
 Nhóm C rất linh động, bạn có thể điều chỉnh độ khó dựa trên tình hình/điều kiện của bạn (ví dụ như "không làm bạn chơi quá chậm"²)
-
+~~
 Sau khi bạn hoàn thành hết nhóm C, hãy tiếp tục luyện tập nhóm A³, đây là một kỹ năng tối quan trọng trong bất kỳ tựa game xếp gạch nào; và bạn sẽ có thể dần dần làm chủ bất kỳ chế độ nào, lúc đó chỉ cần nhìn lướt qua NEXT là đủ rồi.
 
-__________________________
-Chú thích:
-    1: Đề mục này do dịch thuật viên đặt, bản gốc vẫn ghi là Stacking (không hiểu tại sao nhóm A và C bị trùng tên)
-    2: Câu gốc là: like what does “without losing too much speed” means to you (nó giống như việc câu "không làm mất tốc độ của bạn" có ý nghĩa như thế nào đối với bạn)
-    3: Bản gốc ghi là phần A1, nhưng có người bảo nên là nhóm A. (Có thể thay đổi trong tương lai)
-        ]===],
-    },
+~~6
+1: Đề mục này do dịch thuật viên đặt, bản gốc vẫn ghi là Stacking (không hiểu tại sao nhóm A và C bị trùng tên)
+2: Câu gốc là: like what does “without losing too much speed” means to you (nó giống như việc câu "không làm mất tốc độ của bạn" có ý nghĩa như thế nào đối với bạn)
+3: Bản gốc ghi là phần A1, nhưng có người bảo nên là nhóm A. (Có thể thay đổi trong tương lai)
 
---------------------------------------------------------------
+# 20g
+@ title 20G
+Tốc độ nhanh nhất trong các trò Tetris hiện đại. Trong các chế độ xài tốc độ 20G, các miếng gạch thay vì rơi từ từ, nó sẽ xuất hiện ngay lập tức ở cuối bảng. Việc này đôi khi sẽ làm bạn không thể di chuyển được theo phương ngang như ý bạn muốn; bởi vì gạch đôi khi cũng không thể leo qua chỗ lồi lõm hoặc ra khỏi hố sâu. Bạn có thể tìm hiểu thêm về đơn vị \"G\" trong mục \"Tốc độ rơi\"
 
-    ['20g']={
-        title="20G",
-        content="Tốc độ nhanh nhất trong các trò Tetris hiện đại. Trong các chế độ xài tốc độ 20G, các miếng gạch thay vì rơi từ từ, nó sẽ xuất hiện ngay lập tức ở cuối bảng. Việc này đôi khi sẽ làm bạn không thể di chuyển được theo phương ngang như ý bạn muốn; bởi vì gạch đôi khi cũng không thể leo qua chỗ lồi lõm hoặc ra khỏi hố sâu. Bạn có thể tìm hiểu thêm về đơn vị \"G\" trong mục \"Tốc độ rơi\"",
-    },
 
-    hypertap={
-        title="Hypertap",
-        title_full="Hypertap (Nhấn liên tục)",
-        content=[===[
+# hypertap
+@ title Hypertap
+@ title_full Hypertap (Nhấn liên tục)
 Đề cập tới một kỹ năng là khi bạn "rung" tay liên tục để nhấn liên tục làm tốc độ di chuyển ngang¹ nhanh hơn
 
-__________________________
-Chú thích:
-    1:  Di chuyển ngang: di chuyển sang trái và phải
-        ]===],
-    },
+~~6
+--1: Di chuyển ngang: di chuyển sang trái và phải
 
---------------------------------------------------------------
+# 26f_studio
+@ title 26F Studio
+Một nhóm
 
-    ['26f_studio']={
-        title="26F Studio",
-        content="Một nhóm",
-    },
-
---------------------------------------------------------------
-
-    ['keybinding']={
-        title="Gán phím",
-        title_full="Lời khuyên cho việc Thiết lập phím",
-        contentSize=20
-        content=[===[
+# keybinding
+@ title Gán phím
+@ title_full Lời khuyên cho việc Thiết lập phím
+@ contentSize 20
 Dưới đây là vài lời khuyên hữu ích khi bạn thiết lập / gán phím / thiết lập tay cầm của bạn:
 
-1.  Tránh việc gán một ngón tay cho nhiều phím mà bạn muốn nhấn cùng nhau
-      Ví dụ: Bình thường bạn sẽ không cần nhấn nhiều nút xoay chỉ với một ngón tay đâu!
-      Lúc đó bạn nên thử gán mỗi phím cho mỗi ngón tay đi!
-        ]===],
-    },
-}
-
--- Always use font size which can be divided by 5 (ex: 5, 10, 15, 20, 25,...)
+1.Tránh việc gán một ngón tay cho nhiều phím mà bạn muốn nhấn cùng nhau Ví dụ: Bình thường bạn sẽ không cần nhấn nhiều nút xoay chỉ với một ngón tay đâu! Lúc đó bạn nên thử gán mỗi phím cho mỗi ngón tay đi!
+]]
