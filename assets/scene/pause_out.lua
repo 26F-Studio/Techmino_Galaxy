4f13b6d6438b4e634e06f1d04180d2fc99322c70
MrZ_26 <1046101471@qq.com>
2023-05-05 20:01:43 +0800

    暂停界面新增两个音效

--- assets/scene/pause_out.lua
+++ assets/scene/pause_out.lua
@@ -19,9 +19,10 @@ local function sysAction(action)
         SFX.play('unpause')
         SCN.swapTo('game_out','none')
     elseif action=='restart' then
-        SFX.play('fail')
+        SFX.play('pause_restart')
         SCN.swapTo('game_out',nil,GAME.mode.name)
     elseif action=='setting' then
+        SFX.play('pause_setting')
         SCN.go('setting_out')
     end
 end
@@ -64,8 +65,8 @@ end
 
 scene.widgetList={
     WIDGET.new{type='button',pos={0,0},  x= 120,y= 80, w=160,h=80,sound='button_back',fontSize=60,text=CHAR.icon.back,code=WIDGET.c_backScn()},
-    WIDGET.new{type='button',pos={.5,.5},x= 0,  y=-160,w=180,h=90,fontSize=60,text=CHAR.icon.retry,    code=function() sysAction('restart') end},
-    WIDGET.new{type='button',pos={.5,.5},x=-110,y= 170,w=180,h=90,fontSize=60,text=CHAR.icon.play,     code=function() sysAction('back') end},
-    WIDGET.new{type='button',pos={.5,.5},x= 110,y= 170,w=180,h=90,fontSize=60,text=CHAR.icon.settings, code=function() sysAction('setting') end},
+    WIDGET.new{type='button',pos={.5,.5},x= 0,  y=-160,w=180,h=90,fontSize=60,sound=false,text=CHAR.icon.retry,    code=function() sysAction('restart') end},
+    WIDGET.new{type='button',pos={.5,.5},x=-110,y= 170,w=180,h=90,fontSize=60,sound=false,text=CHAR.icon.play,     code=function() sysAction('back') end},
+    WIDGET.new{type='button',pos={.5,.5},x= 110,y= 170,w=180,h=90,fontSize=60,sound=false,text=CHAR.icon.settings, code=function() sysAction('setting') end},
 }
 return scene
