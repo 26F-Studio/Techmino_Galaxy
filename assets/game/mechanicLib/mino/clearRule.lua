c81aaa9ac23dd6d67d8af6e05cd44287bbc3e4f4
MrZ_26 <1046101471@qq.com>
2023-05-15 20:56:29 +0800

    整理代码 框架跟进

--- assets/game/mechanicLib/mino/clearRule.lua
+++ assets/game/mechanicLib/mino/clearRule.lua
@@ -2,8 +2,22 @@ local ins,rem=table.insert,table.remove
 
 local clearRule={}
 
+local function setBias(P,x,y,dx,dy,moveType,clearDelay)
+    if dx==0 and dy==0 then return end
+    local v
+    if moveType=='lineBack' then
+        v=40/clearDelay*dy
+    elseif moveType=='teleBack' then
+        v=clearDelay
+    elseif moveType=='expBack' then
+        v=math.log(40*(dx^2+dy^2)^.5)/clearDelay
+    end
+    P:setCellBias(x,y,{x=dx,y=dy,[moveType]=v})
+end
+
 do-- none
     clearRule.none={}
+    function clearRule.none.getDelay() return 0 end
     clearRule.none.getFill=NULL
     clearRule.none.clear=NULL
 end
@@ -37,18 +51,6 @@ do-- line
         end
     end
 
-    local function setBias(P,x,y,dx,dy,moveType,clearDelay)
-        if dx==0 and dy==0 then return end
-        local v
-        if moveType=='lineBack' then
-            v=40/clearDelay*dy
-        elseif moveType=='teleBack' then
-            v=clearDelay
-        elseif moveType=='expBack' then
-            v=math.log(40*(dx^2+dy^2)^.5)/clearDelay
-        end
-        P:setCellBias(x,y,{x=dx,y=dy,[moveType]=v})
-    end
     function clearRule.line.clear(P,lines)
         local F=P.field
         local sum=0
@@ -68,6 +70,73 @@ do-- line
             F:removeLine(lines[i])
         end
     end
+end
+
+do-- triplets (tetr.js)
+    clearRule.triplets={}
+
+    function clearRule.triplets.getDelay(P,lines)
+        return P.settings.clearDelay
+    end
+
+    function clearRule.triplets.isFill(P,y)
+        local F=P.field
+        for x=1,P.settings.fieldW do
+            if not F:getCell(x,y) then
+                return false
+            end
+        end
+        return true
+    end
+
+    function clearRule.triplets.getFill(P)
+        local fullLines={}
+        for y=P.field:getHeight(),1,-1 do
+            if mechLib.mino.clearRule.triplets.isFill(P,y) then
+                ins(fullLines,y)
+            end
+        end
+
+        -- Filter not in triplets
+        -- TODO
+
+        if #fullLines>0 then
+            return fullLines
+        end
+    end
+
+    clearRule.triplets.clear=clearRule.line.clear
+end
+
+do-- cheese
+    clearRule.cheese={}
+
+    clearRule.cheese.getDelay=clearRule.line.getDelay
+
+    function clearRule.cheese.isFill(P,y)
+        local F=P.field
+        local count=0
+        for x=1,P.settings.fieldW do
+            if not F:getCell(x,y) then
+                count=count+1
+            end
+        end
+        return count<=P.settings.fieldW/10
+    end
+
+    function clearRule.cheese.getFill(P)
+        local fullLines={}
+        for y=P.field:getHeight(),1,-1 do
+            if mechLib.mino.clearRule.cheese.isFill(P,y) then
+                ins(fullLines,y)
+            end
+        end
+        if #fullLines>0 then
+            return fullLines
+        end
+    end
+
+    clearRule.cheese.clear=clearRule.line.clear
 end
 
 return clearRule
