66eeac3a1b1fcdafa9e00e8c3d7aced4cd2b3f90
MrZ_26 <1046101471@qq.com>
2023-06-08 15:48:09 +0800

    调整地图上消四困难模式的位置，添加超音速lo解锁消四困难的方法

--- assets/game/mechanicLib/mino/progress.lua
+++ assets/game/mechanicLib/mino/progress.lua
@@ -25,9 +25,16 @@ do-- hypersonic_lo
             BGM.set(bgmList['secret8th'].add,'volume',math.min((P.modeData.point-bgmTransBegin)/(bgmTransFinish-bgmTransBegin),1),1)
         end
     end
-    function progress.hypersonic_lo_afterClear(P)
-        if P.modeData.point==500 then
+    function progress.hypersonic_lo_afterClear(P,clear)
+        if clear.line>=4 then
+            P.modeData.hypersonic_lo_techrash=(P.modeData.hypersonic_lo_techrash or 0)+1
+            if P.modeData.hypersonic_lo_techrash==36 then
+                PROGRESS.setMinoModeUnlocked('techrash_hard')
+            end
+        end
+        if P.modeData.point>=500 then
             PROGRESS.setMinoModeUnlocked('hypersonic_hi')
+            return true
         end
     end
 end
@@ -40,13 +47,13 @@ do-- hypersonic_hi
         end
     end
     function progress.hypersonic_hi_afterClear(P,clear)
-        if clear.line==4 then
+        if clear.line>=4 then
             P.modeData.hypersonic_hi_techrash=(P.modeData.hypersonic_hi_techrash or 0)+1
             if P.modeData.hypersonic_hi_techrash==42 then
                 PROGRESS.setMinoModeUnlocked('hypersonic_hd')
             end
         end
-        if P.modeData.point==1000 then
+        if P.modeData.point>=1000 then
             PROGRESS.setMinoModeUnlocked('hypersonic_ti')
             return true
         end
