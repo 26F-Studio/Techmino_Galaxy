a872c29f75d3853b4a8e62d0eea6ad99320315a1
MrZ_26 <1046101471@qq.com>
2023-06-11 03:26:59 +0800

    消失40行改为随着块数逐渐加快隐形速度

--- assets/game/mode/mino/exterior/sprint_hide_40.lua
+++ assets/game/mode/mino/exterior/sprint_hide_40.lua
@@ -7,10 +7,13 @@ return {
         playBgm('race','base')
     end,
     settings={mino={
-        pieceVisTime=2000,
-        pieceFadeTime=1500,
         event={
             playerInit=mechLib.mino.statistics.event_playerInit,
+            afterSpawn=function(P)
+                local pieceCount=MATH.clamp(#P.dropHistory,0,99)
+                P.settings.pieceVisTime=math.floor(MATH.interpolate(pieceCount,0,6e3,99,2e3))
+                P.settings.pieceFadeTime=math.floor(MATH.interpolate(pieceCount,0,3e3,99,1e3))
+            end,
             afterClear={
                 mechLib.mino.statistics.event_afterClear,
                 mechLib.mino.sprint.event_afterClear[40],
