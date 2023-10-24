988ac92dbfb432dee05267f0576362e8e270be49
MrZ_26 <1046101471@qq.com>
2023-10-24 15:38:59 +0800

    临时实现ac模式

--- assets/game/mode/mino/exterior/ac_easy.lua
+++ assets/game/mode/mino/exterior/ac_easy.lua
@@ -1,3 +1,26 @@
 --- @type Techmino.Mode
 return {
+    initialize=function()
+        GAME.newPlayer(1,'mino')
+        GAME.setMain(1)
+        playBgm('truth','full')
+    end,
+    settings={mino={
+        nextSlot=8,
+        clearDelay=260,
+        seqType=function()
+        end,
+        event={
+            playerInit=function(P)
+                P.settings.das=math.max(P.settings.das,100)
+                P.settings.arr=math.max(P.settings.arr,20)
+                mechLib.mino.acGenerator.newQuestion(P,{debugging=false,pieceCount=6,holdUsed=true})
+            end,
+            afterClear=function(P)
+                if P.field:getHeight()==0 then
+                    mechLib.mino.acGenerator.newQuestion(P,{debugging=false,pieceCount=6,holdUsed=true})
+                end
+            end,
+        },
+    }},
 }
