8a275d9507b657db5457598ccc465562e04b26f3
MrZ_26 <1046101471@qq.com>
2024-04-24 07:58:08 +0800

    修改race_delay模式引用的音乐名

--- assets/game/mode/mino/exterior/sprint_delay_20.lua
+++ assets/game/mode/mino/exterior/sprint_delay_20.lua
@@ -3,7 +3,7 @@ return {
     initialize=function()
         GAME.newPlayer(1,'mino')
         GAME.setMain(1)
-        playBgm('race_delay')
+        playBgm('race_old')
     end,
     settings={mino={
         inputDelay=620,
