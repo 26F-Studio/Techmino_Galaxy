1dac7fe0369b44df336b497b76bf1a085f2d8e26
MrZ_26 <1046101471@qq.com>
2024-11-21 02:07:50 +0800

    让shader兼容网页端 框架跟进

--- assets/shader/gaussianSharp.hlsl
+++ assets/shader/gaussianSharp.hlsl
@@ -4,13 +4,17 @@ extern float smpCount; // 10
 extern float radius; // 0.026
 extern float intensity; // 1
 
+const float MAX_ITERATIONS = 100.0;
+
 vec4 effect(vec4 color, sampler2D tex, vec2 texCoord, vec2 scrCoord) {
     vec4 normal = texture2D(tex, texCoord);
     vec4 res = vec4(0.0);
     float weightSum = 0.0;
 
-    for (float x = 0.0; x < smpCount; x++) {
-        for (float y = 0.0; y < smpCount; y++) {
+    for (float x = 0.0; x < MAX_ITERATIONS; x++) {
+        if (x >= smpCount) break;
+        for (float y = 0.0; y < MAX_ITERATIONS; y++) {
+            if (y >= smpCount) break;
             vec2 kuvCentered = vec2(x, y) / smpCount - 0.5; // uv coord within the kernel
 
             float bell = cos(length(kuvCentered) * 3.141592653589793) * 0.5 + 0.5;
